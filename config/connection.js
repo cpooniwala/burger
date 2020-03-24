543600d0795dd3af0aaf5fe9388a480b97ca7c70
cpooniwala <cpooniwala@gmail.com>
2020-03-24 11:06:46 -0500

    Heroku config.json file

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "burgerDB"
+  user: "r29ao9fk8h50h0oc",
+  password: "ln13qkbesfs64h2i",
+  database: "wjza7yynrgj78lyz"
 });
 
 // Make connection.
